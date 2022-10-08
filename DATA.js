9ffa7e78bebd2d3247f8dfefba3da9ff4d4ffb63
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-10-08 15:07:21 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "c71c7c41-a4e7-46fc-b9ce-becabbc3ae5a",
+    "home_feed": [
+        {
+            "id": "c0bde44e-d061-4531-8928-eb2a3ee41746",
+            "artist_name": "serve",
+            "home_carousel": [
+                {
+                    "id": "be5d2f5f-6549-4fdd-b913-8101a07ec5b4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/81.png"
+                },
+                {
+                    "id": "2aa9efd9-eab5-4d45-a138-4321a1a843cd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/354.png"
+                },
+                {
+                    "id": "2f815f5a-fa1e-4fb6-81bd-2b40e35f1fb6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/127.png"
+                },
+                {
+                    "id": "a19fea25-b081-43b0-9d81-520dbbeb19bf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/205.png"
+                },
+                {
+                    "id": "1c02bf81-5a13-4d86-b6e4-b37e65ea0b14",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/52.png"
+                }
+            ],
+            "art_caption": "society",
+            "art_description": "Answer herself perhaps instead prepare amount grow more. Song now need bill herself."
+        },
+        {
+            "id": "27e75a2a-7cc0-4d61-a90e-065ee24bd76a",
+            "artist_name": "test",
+            "home_carousel": [
+                {
+                    "id": "30da51e3-eb19-4711-898f-e5291383fa97",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/346.png"
+                },
+                {
+                    "id": "326d88b4-9b46-4f7b-9919-944282331ccc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/164.png"
+                },
+                {
+                    "id": "4a1b0794-768e-4d83-9069-3c50919759fc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/159.png"
+                },
+                {
+                    "id": "c2ff7642-44ec-443a-80e2-abe3b66ade90",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/55.png"
+                },
+                {
+                    "id": "92e0ea6f-a0ff-4f9c-9957-3311e247ef08",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/318.png"
+                }
+            ],
+            "art_caption": "hear",
+            "art_description": "Large send company world. Reduce every there over kitchen."
+        },
+        {
+            "id": "0afc590c-f52b-4aee-94f7-91454207dcf8",
+            "artist_name": "recently",
+            "home_carousel": [
+                {
+                    "id": "a3096e83-fe45-4a38-98e0-dd17bdc5fdad",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/15.png"
+                },
+                {
+                    "id": "a9655e0f-5a03-4632-968f-0dfa48ae38d2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/174.png"
+                },
+                {
+                    "id": "693d6798-6e5c-4109-9e29-4a7c9d7fc40c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/214.png"
+                },
+                {
+                    "id": "2584a9f6-efcc-4a75-80ac-ea4e335e24e1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/77.png"
+                },
+                {
+                    "id": "d96b06c2-6449-4afe-be72-68e5804bc173",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/361.png"
+                }
+            ],
+            "art_caption": "agent",
+            "art_description": "Third us attorney follow trade research soldier. Describe nothing make of."
+        },
+        {
+            "id": "6a27895d-68a0-49be-a8f8-e19163efef00",
+            "artist_name": "rich",
+            "home_carousel": [
+                {
+                    "id": "960ba0a1-fc6f-4d42-8563-02332a37f939",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/29.png"
+                },
+                {
+                    "id": "3497cf43-6e2d-42c9-a9fe-e715ed0430fb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/352.png"
+                },
+                {
+                    "id": "c82696e7-4153-43ce-9ae3-7fd1e7166816",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/176.png"
+                },
+                {
+                    "id": "1cd1502f-8040-48c2-8280-fc105d470abe",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/306.png"
+                },
+                {
+                    "id": "74d8b2a7-d577-4e44-aec4-45a90f0825ec",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/1.png"
+                }
+            ],
+            "art_caption": "little",
+            "art_description": "Rise like serious. Generation American scientist remember choose share. Put side office bad help."
+        },
+        {
+            "id": "4434800b-450e-46d4-9a9d-fef4d0bdda0b",
+            "artist_name": "piece",
+            "home_carousel": [
+                {
+                    "id": "993fcbf7-e1cb-4420-9c99-6797fbefc194",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/25.png"
+                },
+                {
+                    "id": "eb74e299-9315-46d9-a1fd-ff30b5e8ba6e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/344.png"
+                },
+                {
+                    "id": "ba046a78-bab8-407c-bc20-8bf945473db8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/197.png"
+                },
+                {
+                    "id": "30bf0dec-1bc2-4087-a7fc-66eb08784dce",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/120.png"
+                },
+                {
+                    "id": "97f40073-f0ce-4df5-9587-19ab3ee5d014",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/67.png"
+                }
+            ],
+            "art_caption": "particular",
+            "art_description": "Necessary weight point above charge particular wrong. With option character deal become natural."
+        }
+    ]
+}
